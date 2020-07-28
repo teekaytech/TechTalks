6b6e92e5b2173e8080cb8030dd0f8fe2d92fa142
teekaytech <taofeek.olalere@feghas.com>
2020-07-28 11:31:47 +0100

    Create initial models and migrations

---
+++ db/migrate/20200728102122_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.references :author, null: false, index: true
+      t.string :title
+      t.text :text
+      t.string :image
+
+      t.timestamps
+    end
+    add_foreign_key :articles, :users, column: :author_id
+  end
+end
