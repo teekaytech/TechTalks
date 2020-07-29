db7dff82805c4453ec87582724ce9e9b6cfdd78a
teekaytech <taofeek.olalere@feghas.com>
2020-07-29 17:30:45 +0100

    Complete the footer section

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,6 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user
+  def show
+    @category = Category.includes(:articles).find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/signup', to: 'users#new'
-  resources :users, only: [:index, :new, :create, :show, :destroy]
-
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-    
+
+  resources :users, only: %i[index new create show destroy]
+  resources :categories, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
